ff61a2abea482c87367ccc963e8e99b0e2e9213b
Robin Douglas <robin.1996@live.co.uk>
2020-02-05 19:30:50 +0100

    Initial Commit

---
+++ Simple Weather/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Simple Weather
+//
+//  Created by Robin Douglas on 05/02/2020.
+//  Copyright © 2020 Robin Douglas. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
